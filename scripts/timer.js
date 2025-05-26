6a90d3bdcb38b3cb0421730b3b5328848322b6a9
Zacharias King <zacking1998@gmail.com>
2025-05-27 00:23:28 +0100

    styling to create circle around timer and removed seperate start and pause buttons

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,19 +1,22 @@
 let timer;
 
 // Function to start a countdown timer of 2 hours
-document.getElementById("start-button").onclick = function () {
+document.getElementById("timer-circle").onclick = function () {
+  //If the timer is already running, pause it; if it exists but is paused then resume, else start a new timer
   if (timer) {
-    timer.resume();
+    if (timer.isPaused) {
+      timer.resume();
+    } else {
+      timer.pause();
+    }
+  } else {
+    timer = startTimer(7200, "timer-p", function () {});
   }
-  else {
-  timer = startTimer(7200, "timer-p", function () {})
 };
-};
-// Function to pause the timer
-document.getElementById("pause-button").onclick = function () {
-  if (timer) {
-    timer.pause();
-  }
+
+//Function to clear the timer when the clear button is clicked
+document.getElementById("timer-clear").onclick = function () {
+  clearCurrentTimer();
 };
 
 // Function to start a countdown timer
@@ -24,11 +27,13 @@ function startTimer(seconds, container, oncomplete) {
     ms = seconds * 1000,
     display = document.getElementById(container);
   obj = {};
+  obj.isPaused = false;
   obj.resume = function () {
     startTime = new Date().getTime();
     timer = setInterval(obj.step, 250);
   };
   obj.pause = function () {
+    isPaused = true;
     ms = obj.step();
     clearInterval(timer);
   };
@@ -53,9 +58,19 @@ function startTimer(seconds, container, oncomplete) {
   return obj;
 }
 
+
 // Function to add hours to a date object
 function addHours(date, hours) {
   const hoursToAdd = hours * 60 * 60 * 1000;
   date.setTime(date.getTime() + hoursToAdd);
   return date;
 }
+
+// Function to clear the current timer
+function clearCurrentTimer() {
+  if (timer) {
+    timer = null;
+    document.getElementById("timer-p").innerHTML = "00h:00m:00s";
+  }
+};
+
